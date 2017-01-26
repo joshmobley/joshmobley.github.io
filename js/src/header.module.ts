447440d0a085b44b1ba5b889d0e381f488dd086d
Josh Mobley <josh@newkind.com>
2017-01-26 11:50:58 -0500

    adding 404 and fixing meta description issue

--- js/src/header.module.ts
+++ js/src/header.module.ts
@@ -12,6 +12,31 @@ export default function(){
         event.preventDefault();
         $('nav').toggleClass('is-visible');
         $('.menu-toggle').toggleClass('is-fixed');
-    })
+    });
+
+
+    /*let headHeight = $('.small-header').outerHeight();
+
+    $(window).on('scroll', function(){
+        let scrollPos = $(window).scrollTop();
+
+        if( scrollPos > headHeight + 200 ){
+            $('body').css("paddingTop", headHeight );
+            $('.small-header').css({
+                "top": '-' + headHeight + 'px',
+                "position": "fixed",
+            }).animate({
+                "top": "0px"
+            });
+
+        }else if( scrollPos > 150 ){
+            $('body').css("paddingTop","");
+            $('.small-header').animate({
+                "top": "-" + headHeight + 'px'
+            });
+        }
+    });*/
+        
+
 
 }
